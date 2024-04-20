8951595e37a4fe58c2a6bf26d726d21b76b294ed
Kevin Ju <kevjuliow@gmail.com>
2024-04-20 22:55:24 +0700

    add user session

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/session"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"net/http"
@@ -21,8 +22,13 @@ var googleOauthConfig = &oauth2.Config{
 }
 
 func (s *FiberServer) RegisterFiberRoutes() {
+	s.Store = session.New(session.Config{
+		Expiration:   3600,
+		CookieSecure: false, //if true , it will only send to https and not http
+	})
 	s.App.Get("/auth/google", s.authGoogleHandler)
 	s.App.Get("/auth/google/callback", s.authGoogleCallbackHandler)
+	s.App.Get("/profile", s.profileHandler)
 }
 
 func (s *FiberServer) authGoogleHandler(c *fiber.Ctx) error {
@@ -33,31 +39,47 @@ func (s *FiberServer) authGoogleHandler(c *fiber.Ctx) error {
 
 func (s *FiberServer) authGoogleCallbackHandler(c *fiber.Ctx) error {
 	log.Info("context", c)
-	//state := c.Query("state")
 
-	//var urlPath string = "/"
-	//if state != "" {
-	//	urlPath = state
-	//}
+	var pathUrl string = "/"
+	state := c.Query("state")
+	if state != "" {
+		pathUrl = state
+	}
 
 	code := c.Query("code")
 	if code == "" {
 		c.SendStatus(http.StatusUnauthorized)
 	}
 
+	//get token
 	token, err := googleOauthConfig.Exchange(context.Background(), code)
 	if err != nil {
 		c.SendStatus(http.StatusUnauthorized)
 	}
 
+	//exchange token with userInfo
 	userInfoJSON, err := fetchGoogleUserInfo(token)
 	if err != nil {
 		c.SendStatus(http.StatusInternalServerError)
 	}
-	return c.JSON(fiber.Map{
-		"status": "Success",
-		"data":   userInfoJSON,
-	})
+
+	//save userInfo to session
+	//Because session cant keep large files , so json needs to be turn to bytes
+	jsonMarshal, err := json.Marshal(userInfoJSON)
+	if err != nil {
+		return err
+	}
+	session, err := s.Store.Get(c)
+	if err != nil {
+		return err
+	}
+	session.Set("user", jsonMarshal)
+
+	if err := session.Save(); err != nil {
+		return err
+	}
+
+	return c.Redirect(fmt.Sprint("http://localhost:8000", pathUrl), http.StatusTemporaryRedirect)
 }
 
 func fetchGoogleUserInfo(token *oauth2.Token) (map[string]interface{}, error) {
@@ -77,4 +99,28 @@ func fetchGoogleUserInfo(token *oauth2.Token) (map[string]interface{}, error) {
 
 	fmt.Println(userInfoJSON)
 	return userInfoJSON, nil
+}
+
+func (s *FiberServer) profileHandler(c *fiber.Ctx) error {
+	session, err := s.Store.Get(c)
+	if err != nil {
+		return err
+	}
+
+	//get from session key "user"
+	userMarshal := session.Get("user")
+	if userMarshal == nil {
+		return c.SendStatus(http.StatusUnauthorized)
+	}
+
+	//unMarshal the userInfo
+	var userJSON interface{}
+	if err := json.Unmarshal(userMarshal.([]byte), &userJSON); err != nil {
+		return err
+	}
+
+	return c.JSON(fiber.Map{
+		"status": "ok",
+		"data":   userJSON,
+	})
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,14 +2,15 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
 
 	"gofiber-oauth/internal/database"
 )
 
 type FiberServer struct {
 	*fiber.App
-
-	db database.Service
+	Store *session.Store
+	db    database.Service
 }
 
 func New() *FiberServer {
